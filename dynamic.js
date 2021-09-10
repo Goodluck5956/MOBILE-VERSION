321b0367012f017f9f9b5848e750d73ca626f9a9
OSUAGWU <osuagwulucky.081@gmail.com>
2021-09-10 21:49:36 +0100

    fix linters

--- dynamic.js
+++ dynamic.js
@@ -1,27 +1,28 @@
+/* eslint-disable linebreak-style */
 const menu = document.querySelector('.mobile-menu');
 const ham = document.querySelector('.ham');
 const xIcon = document.querySelector('.xIcon');
 const menuIcon = document.querySelector('.menuIcon');
-const main = document.querySelector('.main');
+// const main = document.querySelector('.main');
 // const logo = document.querySelector('.bold');
-const header = document.getElementById('logo');
-const projs = document.querySelectorAll('.work');
+// const header = document.getElementById('logo');
+// const projs = document.querySelectorAll('.work');
 
-function projOn() {
-  projs.forEach(
-    (work) => {
-      work.style.filter = 'blur(4px)';
-    },
-  );
-}
+// function projOn() {
+//   projs.forEach(
+//     (work) => {
+//       work.style.filter = 'blur(4px)';
+//     },
+//   );
+// }
 
-function projOff() {
-  projs.forEach(
-    (work) => {
-      work.style.filter = 'blur(0px)';
-    },
-  );
-}
+// function projOff() {
+//   projs.forEach(
+//     (work) => {
+//       work.style.filter = 'blur(0px)';
+//     },
+//   );
+// }
 
 function toggleMenu() {
   if (menu.classList.contains('showMenu')) {
@@ -44,3 +45,4 @@ menuLinks.forEach(
     mobileLink.addEventListener('click', toggleMenu);
   },
 );
+/* eslint-enable linebreak-style */
