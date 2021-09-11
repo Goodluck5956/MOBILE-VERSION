9262438e5869a0d216a50833b57e22c9a9238b2a
OSUAGWU <osuagwulucky.081@gmail.com>
2021-09-11 22:43:17 +0100

    fix linters

--- dynamic.js
+++ dynamic.js
@@ -3,26 +3,7 @@ const menu = document.querySelector('.mobile-menu');
 const ham = document.querySelector('.ham');
 const xIcon = document.querySelector('.xIcon');
 const menuIcon = document.querySelector('.menuIcon');
-// const main = document.querySelector('.main');
-// const logo = document.querySelector('.bold');
-// const header = document.getElementById('logo');
 const projs = document.querySelectorAll('.work');
-
-// function projOn() {
-projs.forEach(
-  (work) => {
-    work.style.filter = 'blur(4px)';
-  },
-);
-// }
-
-// function projOff() {
-projs.forEach(
-  (work) => {
-    work.style.filter = 'blur(0px)';
-  },
-);
-// }
 
 function toggleMenu() {
   if (menu.classList.contains('showMenu')) {
